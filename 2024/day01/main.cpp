6124cb5486b4800290f4b13d0a3a23037890a99e
komorebi <komorebi@troicinet.mynetworksettings.com>
2024-12-17 01:07:49 -0500

    use std namespace

--- 2024/day01/main.cpp
+++ 2024/day01/main.cpp
@@ -9,25 +9,27 @@
 #include <tuple>
 #include <vector>
 
-void readFile(const std::string& filename, std::vector<int>& v1, std::vector<int>& v2);
-void printData(const std::vector<int>& v1, const std::vector<int>& v2);
-void part1(const std::vector<int>& v1, const std::vector<int>& v2);
-void part2(const std::vector<int>& v1, const std::vector<int>& v2);
+using namespace std;
+
+void readFile(const string& filename, vector<int>& v1, vector<int>& v2);
+void printData(const vector<int>& v1, const vector<int>& v2);
+void part1(const vector<int>& v1, const vector<int>& v2);
+void part2(const vector<int>& v1, const vector<int>& v2);
 
 int main(int argc, char* argv[]) {
-    std::vector<std::string> args(argv + 1, argv + argc);
+    vector<string> args(argv + 1, argv + argc);
 
     for (auto arg : args) {
-        std::cout << "File " << arg << "\n";
+        cout << format("File {}\n", arg);
 
-        std::vector<int> v1;
-        std::vector<int> v2;
+        vector<int> v1;
+        vector<int> v2;
 
         readFile(arg, v1, v2);
 //        printData(v1, v2);
 
-        std::sort(v1.begin(), v1.end());
-        std::sort(v2.begin(), v2.end());
+        sort(v1.begin(), v1.end());
+        sort(v2.begin(), v2.end());
 
         part1(v1, v2);
         part2(v1, v2);
@@ -37,14 +39,14 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void readFile(const std::string& filename, std::vector<int>& v1, std::vector<int>& v2) {
-    std::ifstream puzzle_in(filename);
-    std::string line;
+void readFile(const string& filename, vector<int>& v1, vector<int>& v2) {
+    ifstream puzzle_in(filename);
+    string line;
 
-    while (std::getline(puzzle_in, line)) {
+    while (getline(puzzle_in, line)) {
         int n1;
         int n2;
-        std::istringstream line_stream(line);
+        istringstream line_stream(line);
 
         line_stream >> n1;
         line_stream >> n2;
@@ -53,28 +55,28 @@ void readFile(const std::string& filename, std::vector<int>& v1, std::vector<int
     }
 }
 
-void part1(const std::vector<int>& v1, const std::vector<int>& v2) {
+void part1(const vector<int>& v1, const vector<int>& v2) {
     size_t distance = 0;
-    for (const auto& [a,b] : std::views::zip(v1, v2)) {
-        distance += std::abs(a - b);
+    for (const auto& [a,b] : views::zip(v1, v2)) {
+        distance += abs(a - b);
     }
 
-    std::cout << std::format("Part 1: {}\n", distance);
+    cout << format("Part 1: {}\n", distance);
 }
 
-void part2(const std::vector<int>& v1, const std::vector<int>& v2) {
+void part2(const vector<int>& v1, const vector<int>& v2) {
     size_t similarity = 0;
 
     for (const auto& a : v1) {
-        auto num = std::ranges::count(v2.begin(), v2.end(), a);
+        auto num = ranges::count(v2.begin(), v2.end(), a);
         similarity += num * a;
     }
 
-    std::cout << std::format("Part 2: {}\n", similarity);
+    cout << format("Part 2: {}\n", similarity);
 }
 
-void printData(const std::vector<int>& v1, const std::vector<int>& v2) {
-    for (const auto& [a,b] : std::views::zip(v1, v2)) {
-        std::cout << std::format("{} {}\n", a, b);
+void printData(const vector<int>& v1, const vector<int>& v2) {
+    for (const auto& [a,b] : views::zip(v1, v2)) {
+        cout << format("{} {}\n", a, b);
     }
 }
