4a7d081ebce5805492bd257686576ab2bd0900d9
komorebi <komorebi@troicinet.mynetworksettings.com>
2024-12-17 02:55:06 -0500

    day 3 part 2

--- 2024/day03/main.cpp
+++ 2024/day03/main.cpp
@@ -10,7 +10,9 @@ using namespace std;
 
 void readFile(const string& filename, vector<string>& vs);
 void printData(vector<string>& vs);
-void part1(vector<string>& vs);
+void part1(const vector<string>& vs);
+void part2(vector<string>& vs);
+size_t convertAndMult(string x, string y);
 
 int main(int argc, char* argv[]) {
     vector<string> args(argv + 1, argv + argc);
@@ -23,6 +25,7 @@ int main(int argc, char* argv[]) {
 //        printData(vs);
 
         part1(vs);
+        part2(vs);
     }
 
     return 0;
@@ -43,9 +46,45 @@ void printData(vector<string>& vs) {
     }
 }
 
-void part1(vector<string>& vs) {
+size_t convertAndMult(string x, string y) {
+    int xi;
+    int yi;
+    from_chars(x.data(), x.data() + x.size(), xi);
+    from_chars(y.data(), y.data() + y.size(), yi);
+
+    return xi * yi;
+}
+
+void part1(const vector<string>& vs) {
     size_t total = 0;
     auto const rx = regex(R"(mul\((\d{1,3}),(\d{1,3})\))");
+
+    for (const string& s : vs) {
+        string e(s);
+        for (smatch sm; regex_search(e, sm, rx);) {
+//            cout << format("{} - match size {}\n", sm.str(), sm.size());
+//            for (size_t i = 0; i < sm.size(); ++i) {
+//                cout << sm[i] << "\n";
+//            }
+
+            // on a match, sm is size 3
+            // 0 - entire "mul(x,y)" string
+            // 1 - x
+            // 2 - y
+
+            total += convertAndMult(sm[1], sm[2]);
+
+            e = sm.suffix();
+        }
+    }
+
+    cout << format("Part 1: {}\n", total);
+}
+
+void part2(vector<string>& vs) {
+    size_t total = 0;
+    bool mul = true;
+    auto const rx = regex(R"(mul\((\d{1,3}),(\d{1,3})\)|don\'t\(\)|do\(\))");
 
     for (string& e : vs) {
         for (smatch sm; regex_search(e, sm, rx);) {
@@ -54,22 +93,20 @@ void part1(vector<string>& vs) {
 //                cout << sm[i] << "\n";
 //            }
 
-            // on a match, sm is size 3
-            // 0 - entire mul(x,y) string
-            // 1 - x
-            // 2 - y
-            string x(sm[1]);
-            string y(sm[2]);
-            int xi;
-            int yi;
-            from_chars(x.data(), x.data() + x.size(), xi);
-            from_chars(y.data(), y.data() + y.size(), yi);
-
-            total += xi * yi;
+            string sm_str(sm.str());
+            if (sm_str == "do()") {
+                mul = true;
+            } else if (sm_str == "don\'t()") {
+                mul = false;
+            } else {
+                if (mul) {
+                    total += convertAndMult(sm[1], sm[2]);
+                }
+            }
 
             e = sm.suffix();
         }
     }
 
-    cout << format("Part 1: {}\n", total);
+    cout << format("Part 2: {}\n", total);
 }
